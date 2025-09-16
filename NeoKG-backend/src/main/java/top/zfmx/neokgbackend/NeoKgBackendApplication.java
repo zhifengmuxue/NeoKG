04b48ea945bcceee1865b7fcfea1386dd7180e20
li ma <zhifengmuxue@163.com>
2025-09-16 09:59:15 +0800

    init: 项目初始化

---
+++ NeoKG-backend/src/main/java/top/zfmx/neokgbackend/NeoKgBackendApplication.java
@@ -0,0 +1,13 @@
+package top.zfmx.neokgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoKgBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoKgBackendApplication.class, args);
+    }
+
+}
---
+++ NeoKG-backend/src/test/java/top/zfmx/neokgbackend/NeoKgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zfmx.neokgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoKgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
