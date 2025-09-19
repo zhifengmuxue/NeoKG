2d009f7cdae068fbbd8dd1251f1868f30e60a351
li ma <zhifengmuxue@163.com>
2025-09-19 15:05:55 +0800

    feat: keyword接口完善

--- NeoKG-backend/src/main/java/top/zfmx/neokgbackend/controller/KeywordController.java
+++ NeoKG-backend/src/main/java/top/zfmx/neokgbackend/controller/KeywordController.java
@@ -2,10 +2,7 @@ package top.zfmx.neokgbackend.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.zfmx.neokgbackend.model.Keyword;
 import top.zfmx.neokgbackend.response.Result;
 import top.zfmx.neokgbackend.service.KeywordService;
@@ -31,5 +28,25 @@ public class KeywordController {
     public Result<IPage<Keyword>> getKeywordsPage(@RequestParam(defaultValue = "1") int currentPage,
                                                   @RequestParam(defaultValue = "10") int pageSize) {
         return Result.ok(keywordService.findAllKeywordsPage(currentPage, pageSize));
+    }
+    @DeleteMapping("/batch")
+    public Result<Boolean> deleteKeywords(@RequestBody List<Long> ids) {
+        boolean removed = keywordService.removeBatchByIds(ids);
+        return Result.ok(removed);
+    }
+    @DeleteMapping("/{id}")
+    public Result<Boolean> deleteKeyword(@PathVariable Long id) {
+        boolean removed = keywordService.removeById(id);
+        return Result.ok(removed);
+    }
+    @PutMapping
+    public Result<Boolean> updateKeyword(@RequestBody Keyword keyword) {
+        boolean updated = keywordService.updateById(keyword);
+        return Result.ok(updated);
+    }
+    @PostMapping
+    public Result<Boolean> addKeyword(@RequestBody Keyword keyword) {
+        boolean saved = keywordService.save(keyword);
+        return Result.ok(saved);
     }
 }
